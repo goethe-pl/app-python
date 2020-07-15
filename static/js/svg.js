bfeae3d78a29c42291169928d2fa8ab120f7358a
tom <tom@sapletta.com>
2020-07-15 23:10:37 +0200

    first version

--- static/js/svg.js
+++ static/js/svg.js
@@ -2,9 +2,14 @@ $('#process').on('click', function () {
     updateSVG();
 });
 
-// $(document).ready(function () {
-//     updateSVG();
-// });
+$(document).ready(function () {
+    var sentence = $('#zdanie').val();
+    var varr = sentence.split(' ');
+    console.log(sentence, varr);
+    $('#sentence').val(varr);
+    $('#sentence').amsifySuggestags();
+
+});
 
 function updateSVG() {
     var sentence = $('#zdanie').val();
@@ -17,9 +22,23 @@ function updateSVG() {
         url: "/svg/filename/pl/" + sentence
     }).done(function (data) {
         console.log(data);
-        $('#svg').attr('src', data);
+        $('#svg').attr('src', 'svg/' + data);
 
         // alert('done');
         // $(this).addClass("done");
     });
 }
+
+
+j.obj("json/sentence.json", function (obj, url) {
+    console.log(obj, typeof obj, url);
+    console.log(obj["q&a"]);
+
+    each(obj["q&a"], function (v, k) {
+        var varr = v.split(' ');
+        console.log(k, v, varr);
+        $('#first').val(varr);
+        $('#zdanie').val(k);
+        // $('#process').click();
+    });
+});
