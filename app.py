60ca1daaade5d006dd1018f72e41f65e661cd8df
tom <tom@sapletta.com>
2020-07-18 14:01:28 +0200

    first version

--- app.py
+++ app.py
@@ -13,10 +13,7 @@ from pathlib import Path
 nlp_pl = spacy.load("pl_core_news_md")
 nlp_de = spacy.load("de_core_news_md")
 
-app = Flask(__name__,
-            static_url_path='/',
-            static_folder='/static',
-            )
+app = Flask(__name__, static_url_path='')
 host = '0.0.0.0'
 port = 80
 
